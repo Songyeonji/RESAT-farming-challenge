887ebbc765a1e55ffa8f91505a396dab03e889fd
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-06-21 14:33:05 +0900

    자바스크립트 스톱워치 만들기

--- index.js
+++ index.js
@@ -1,133 +1,69 @@
-/* 발견되면 활성화시키는 라이브러리 시작 */
-function ActiveOnVisible__init() {
-    $(".active-on-visible").each(function (index, node) {
-      var $node = $(node);
-  
-      var onFuncName = $node.attr("data-active-on-visible-on-func-name");
-      var offFuncName = $node.attr("data-active-on-visible-off-func-name");
-  
-      $node.data("data-active-on-visible-on-func-name", onFuncName);
-      $node.data("data-active-on-visible-off-func-name", offFuncName);
-    });
-  
-    $(window).resize(_.debounce(ActiveOnVisible__initOffset, 500));
-    ActiveOnVisible__initOffset();
-  
-    $(window).scroll(_.debounce(ActiveOnVisible__checkAndActive, 50));
-    ActiveOnVisible__checkAndActive();
+let seconds = 0;      // seconds
+let centiseconds = 0; // 1/100 seconds
+let minutes = 0;
+let hours = 0;
+let spanCentiseconds =document.getElementById('centiseconds');
+let spanSeconds = document.getElementById('seconds');
+let spanMinutes = document.getElementById('minutes');
+let spanHours = document.getElementById('hours');
+let buttonStart = document.getElementById('button-start');
+let buttonStop = document.getElementById('button-stop');
+let buttonReset = document.getElementById('button-reset');
+let interval;  // 1/100 초씩 자동실행되는 곳에 사용할 변수
+ 
+// 시작버튼 누를시 실행함수
+buttonStart.onclick = function() {
+  if(buttonStart.disabled == false) {     // start버튼이 활성화 되었다면(여러번 실행되는 것 방지)
+    interval = setInterval(startTimer, 10); // startTimer함수를 10/1000초마다 실행하겠다.
+    buttonStart.disabled = true;      // start버튼을 비활성화 하기
   }
-  
-  function ActiveOnVisible__initOffset() {
-    var windowHeight = $(window).height();
-  
-    $(".active-on-visible:not(.actived)").each(function (index, node) {
-      var $node = $(node);
-  
-      var offsetTop = $node.offset().top;
-  
-      var matrix = $node
-        .css("transform")
-        .replace(/[^0-9\-.,]/g, "")
-        .split(",");
-      var translateX = matrix[12] || matrix[4];
-      var translateY = matrix[13] || matrix[5];
-  
-      if (translateY) {
-        offsetTop -= translateY;
-      }
-  
-      $node.attr("data-active-on-visible-offsetTop", offsetTop);
-      $node.data("data-active-on-visible-offsetTop", offsetTop);
-  
-      if (!$node.attr("data-active-on-visible-diff-y")) {
-        $node.attr("data-active-on-visible-diff-y", "0");
-      }
-  
-      if (!$node.attr("data-active-on-visible-delay")) {
-        $node.attr("data-active-on-visible-delay", "0");
-      }
-  
-      var diffY = $node.attr("data-active-on-visible-diff-y");
-      var delay = $node.attr("data-active-on-visible-delay");
-  
-      if (diffY.substr(-2, 2) == "vh") {
-        diffY = parseInt(diffY);
-  
-        diffY = windowHeight * (diffY / 100);
-      } else if (diffY.substr(-1, 1) == "%") {
-        diffY = parseInt(diffY);
-  
-        diffY = $node.height() * (diffY / 100);
-      } else {
-        diffY = parseInt(diffY);
-      }
-  
-      $node.attr("data-active-on-visible-diff-y-real", diffY);
-      $node.data("data-active-on-visible-diff-y", diffY);
-      $node.data("data-active-on-visible-delay", delay);
-    });
-  
-    ActiveOnVisible__checkAndActive();
+}
+ 
+// 정지버튼을 누를시 실행함수
+buttonStop.onclick = function() {
+  clearInterval(interval); // 1/100초마다 실행되는 함수 멈춤
+  buttonStart.disabled = false; // start 버튼을 활성화 하기
+}
+ 
+// 재시작 버튼 누를시 실행함수
+buttonReset.onclick = function() {
+  buttonStart.disabled == false;  // start 버튼을 활성화하기
+  clearInterval(interval); // 1/100초마다 실행되는 함수 멈춤
+  hours = 0;
+  seconds = 0;
+  minutes = 0;
+  centiseconds = 0;
+  spanCentiseconds.innerText = '00';
+  spanSeconds.innerText = '00';
+  spanMinutes.innerText = '00';
+  spanHours.innerText = '00';
+}
+ 
+function startTimer() { // 1초, 2초......시간 계산하는 함수
+  centiseconds++;  // 1증가 // centiseconds = centiseconds + 1
+  if(centiseconds <= 9) spanCentiseconds.innerText = '0'+centiseconds;
+  else spanCentiseconds.innerText = centiseconds;
+
+  if(centiseconds > 99) {
+    seconds++; // 1초 상승
+    if(seconds <= 9) spanSeconds.innerText = '0' + seconds;
+    else spanSeconds.innerText = seconds;
+    centiseconds = 0;
+    spanCentiseconds.innerText = '00';
   }
-  
-  function ActiveOnVisible__checkAndActive() {
-    $(".active-on-visible:not(.actived)").each(function (index, node) {
-      var $node = $(node);
-  
-      var offsetTop = $node.data("data-active-on-visible-offsetTop") * 1;
-      var diffY = parseInt($node.data("data-active-on-visible-diff-y"));
-      var delay = parseInt($node.data("data-active-on-visible-delay"));
-  
-      var onFuncName = $node.data("data-active-on-visible-on-func-name");
-      var offFuncName = $node.data("data-active-on-visible-off-func-name");
-  
-      var scrollTop = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      var nodeHeight = $node.height();
-  
-      if (scrollTop + windowHeight + diffY > offsetTop) {
-        setTimeout(function () {
-          if ($node.hasClass("active") == false) {
-            $node.addClass("active");
-  
-            if ($node.hasClass("can-active-once")) {
-              $node.addClass("actived");
-            }
-  
-            if (window[onFuncName]) {
-              window[onFuncName]($node);
-            }
-          }
-        }, delay);
-      } else {
-        if ($node.hasClass("active")) {
-          $node.removeClass("active");
-  
-          if (window[offFuncName]) {
-            window[offFuncName]($node);
-          }
-        }
-      }
-    });
+  if(seconds > 59){
+    minutes++;
+    if (minutes <= 9) spanMinutes.innerText = '0' + minutes;
+    else spanMinutes.innerText = minutes;
+    seconds = 0
+    spanSeconds.innerText = '00';
   }
-  
-  $(function () {
-    ActiveOnVisible__init();
-  });
-  /* 발견되면 활성화시키는 라이브러리 끝 */
-  
-  function SliderBox1__init() {
-    $(".slider-box-1 > .slick").slick({
-      dots: false,
-      autoplay: false,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      centerMode: true,
-      centerPadding: 0,
-      arrows: true,
-      prevArrow: ".slider-box-1 > .arrows > .btn-arrow-left",
-      nextArrow: ".slider-box-1 > .arrows > .btn-arrow-right",
-    });
+
+  if(minutes > 59){
+    hours++;
+    if (hours <= 9) spanHours.innerText = '0' + hours;
+    else spanHours.innerText = hours;
+    minutes = 0
+    spanMinutes.innerText = '00';
   }
-  
-  SliderBox1__init();
+}
